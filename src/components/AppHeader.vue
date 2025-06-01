40fcb05a6e47db0846502ca6e160165d9bde4ff3
Sébastien Snaider <sebastien.snaidero@gmail.com>
2025-06-01 16:21:10 +0200

    style: title & icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <v-app-bar title="English Irregular Verbs">
-    <v-spacer />
+  <v-app-bar>
+    <template #title>
+      <v-img
+        inline
+        src="https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg"
+        width="30px"
+      />
+      English Irregular Verbs
+    </template>
     <v-btn v-if="$route.path !== '/exercise'" to="/exercise">Exercise</v-btn>
     <v-chip v-else class="text-button text-primary mx-6">Exercise</v-chip>
   </v-app-bar>
